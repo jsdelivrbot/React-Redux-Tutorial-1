1cc896a7d419c5e92ef45439eb270cfa87fe9eb8
Michael Irby <michael.irbstoomuch@gmail.com>
2017-02-22 09:54:28 -0500

    components added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyA9NqnafyECSp37UBb2tAlLxbrWyV1q3yw';
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-    return <div>Hi!</div>;
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        YTSearch({
+            key: API_KEY,
+            term: 'fail'
+        }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
